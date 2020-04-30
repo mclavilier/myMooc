7513756afe3965b56ad23c2157ef610811b74d7a
mclavilier <64615957+mclavilier@users.noreply.github.com>
2020-05-01 01:10:43 +0200

    Add files via upload

---
+++ svm/Lesson 4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 15:23:59 2020
+
+@author: clavilier
+"""
+
+import math
+r = -0.5*math.log(0.5, 2) - 0.5*math.log(0.5, 2)
+print r
+
+r = -0.666667*math.log(0.666667, 2) - 0.333334*math.log(0.333334, 2)
+r = 0.9184
+r = r*3/4
+r = 1 - r
+print r
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,4 +27,51 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+
+print "start"
+#classifier = SVC(kernel='linear',gamma='auto')
+classifier = SVC(C=10000 ,gamma='auto', kernel='rbf')
+print "Classifier ok"
+
+#Accelerate but which price ?
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+### define T0
+t0 = time()
+print " Classifier under training ... "
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+### define T1
+t1 = time()
+
+print "Prediction on going... "
+predicter = classifier.predict(features_test)
+
+print "Prediction time:", round(time()-t1, 3), "s"
+#pred1 = predicter[10]
+#pred2 = predicter[26]
+#pred3 = predicter[50]
+
+#print pred1
+#print pred2
+#print pred3
+
+#print classifier.class_weight_
+#print classifier.shape_fit_
+#print classifier
+#print classifier.classes_
+#print predicter
+
+print classification_report(labels_test, predicter)
+print "No. of predicted to be in the 'Chris'(1): %r" % sum(predicter)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, predicter)
+
+print accuracy
+
 
