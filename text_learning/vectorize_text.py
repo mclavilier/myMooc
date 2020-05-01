0917b2563d02a1afd024da9eb46af9e17fb01b06
mclavilier <64615957+mclavilier@users.noreply.github.com>
2020-05-01 21:35:57 +0200

    Add files via upload

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
 import pickle
@@ -41,31 +42,37 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-#        temp_counter += 1
-#        if temp_counter < 200:
-        path = os.path.join('..', path[:-1])
-        print path
-        email = open(path, "r")
-
-        ### use parseOutText to extract the text from the opened email
-        text = parseOutText(email)
-        ### use str.replace() to remove any instances of the words
-        ### ["sara", "shackleton", "chris", "germani"]
-        toreplace = ["sara", "shackleton", "chris", "germani"]
-        for t in toreplace:
-            text.replace(t,"")
+        temp_counter += 1
+        if True or temp_counter < 200:
+            path = os.path.join('..', path[:-1])
+            print path
+            email = open(path, "r")
+    
+            ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
+            ### use str.replace() to remove any instances of the words
+            ### ["sara", "shackleton", "chris", "germani"]
             
-        ### append the text to word_data
-#        if len(t) >0:
-        word_data.append(text)
-        ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-        from_data.append(0 if name == "sara" else 1)
-#        if name == "sara":
-#            from_data.append(0)
-#        else:
-#            from_data.append(1)
-
-        email.close()
+            ### For any reason this loop doesn't work as espected
+#            toreplace = ["sara", "shackleton", "chris", "germani"]
+#            for t in toreplace:
+#                text.replace(t,"")
+            text = text.replace("sara","")
+            text = text.replace("shackleton", "")
+            text = text.replace("chris","")
+            text = text.replace("germani","")
+            
+            ### append the text to word_data
+    #        if len(t) >0:
+            word_data.append(text)
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            from_data.append(0 if name == "sara" else 1)
+    #        if name == "sara":
+    #            from_data.append(0)
+    #        else:
+    #            from_data.append(1)
+    
+            email.close()
 
 
 print "emails processed"
@@ -73,7 +80,7 @@ print "emails processed"
 from_sara.close()
 from_chris.close()
 #print "/n"
-#print word_data[152]
+print word_data[152]
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
@@ -91,11 +98,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # As we have to not use NLTK, the sklearn stop_words is used.
  
-vectorizer = TfidfVectorizer(stop_words="english")
+vectorizer = TfidfVectorizer(stop_words=sw)
 bag_words = vectorizer.fit_transform(word_data)
 # bag_words = vectorizer.transform(word_data)
 print len(vectorizer.get_feature_names())
-
+print (bag_words.shape)
 print "Done with transformation"
 #vector = vectorizer.get_feature_names()
 print vectorizer.get_feature_names()[34597]
